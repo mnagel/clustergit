80b7885610741d2013541e31c906ecfaa0e7bf3c
Michael Nagel <michael.nagel@devzero.de>
2014-12-13 21:30:38 +0100

    add testcase for warn-unversioned+exclude (#6)

--- tests/tests/clustergit_test.py
+++ tests/tests/clustergit_test.py
@@ -2,9 +2,9 @@ from tools import same
 
 from cStringIO import StringIO
 import sys
+import tempfile
 
 import clustergit
-
 
 class TestClustergit:
 
@@ -30,8 +30,9 @@ Scanning sub directories of .
         sys.stdout = old_stdout
         sys.stderr = old_stderr
 
+
+
     def test_check_fresh_repo(self):
-        import tempfile
         dirpath = tempfile.mkdtemp()
         print "working in %s" % (dirpath)
         clustergit.run('cd "%s"; mkdir mygit; cd mygit; git init' % (dirpath), clustergit.read_arguments(['-v']))
@@ -53,6 +54,44 @@ Done
         expected_err = """"""
         same("stdout should be alright", expected_out, actual_out)
         same("stderr should be alright", expected_err, actual_err)
+
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+
+
+
+    def test_excluded(self):
+        dirpath = tempfile.mkdtemp()
+        print "working in %s" % (dirpath)
+        out = clustergit.run(
+            'cd "%s";' % (dirpath)
+            + 'mkdir notarepo repo1 repo2 target;'
+            + 'cd repo1; git init; cd ..;'
+            + 'cd repo2; git init; cd ..;'
+            + 'tree -A', # show structure in error messages
+            clustergit.read_arguments(['-v'])
+        )
+        print out
+
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = mystdout = StringIO()
+        sys.stderr = mystderr = StringIO()
+
+        clustergit.main(['-d', dirpath, '--no-color', '--align=0', '--warn-unversioned', '--exclude=target'])
+
+        actual_out = mystdout.getvalue()
+        expected_out = """Starting git status...
+Scanning sub directories of {0}
+{0}/notarepo: Not a GIT repository
+{0}/repo1: Changes
+{0}/repo2: Changes
+Done
+""".format(dirpath)
+        actual_err = mystderr.getvalue()
+        expected_err = ''
+        same("stdout should exclude target", expected_out, actual_out)
+        same("stderr should be empty", expected_err, actual_err)
 
         sys.stdout = old_stdout
         sys.stderr = old_stderr
