130cc56b7f53dc672ebcfa1791c72fa9a3f305e8
Michael Nagel <michael.nagel@devzero.de>
2014-12-13 21:29:50 +0100

    more helpful error message

--- tests/tests/tools.py
+++ tests/tests/tools.py
@@ -1,7 +1,7 @@
 from nose.tools import *
 
 def same(message, expected, actual):
-    eq_(actual, expected, '%s. We expected %s but got %s' % (message, expected, actual))
+    eq_(actual, expected, '%s. We expected [%s] but got [%s]' % (message, expected, actual))
 
 def confirm(message, actual):
 	eq_(message, True, actual)
